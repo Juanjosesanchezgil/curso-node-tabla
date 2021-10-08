51501e6b091bbaab0e795602452d1e1d5b7d8f0a
Juan Sanchez <juanjosesanchezgil@gmail.com>
2021-10-08 16:45:45 +0200

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const colors = require("colors");
+
+const crearArchivo = async (base = 5, listar = false, hasta = 10) => {
+  try {
+    let salida = "";
+    let consola = "";
+
+    for (let i = 1; i <= hasta; ++i) {
+      salida += `${base} x ${i} = ${base * i}\n`;
+      consola += `${base} ${"x".green} ${i} ${"=".green} ${base * i}\n`;
+    }
+    if (listar) {
+      console.log("===================".green);
+      console.log("   Tabla del : ".green, colors.blue(base));
+      console.log("===================".green);
+      console.log(consola);
+    }
+
+    // fs.writeFile(`tabla-${tabla}.txt`, salida, (err) => {
+    //   if (err) throw err;
+
+    //   console.log(`tabla-${tabla}.txt creado`);
+    // });
+
+    fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+
+    return `table-${base}.txt creado`;
+  } catch (error) {
+    throw err;
+  }
+};
+
+module.exports = {
+  crearArchivo,
+};
